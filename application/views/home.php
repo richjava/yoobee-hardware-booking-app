2990cc8e7fb25361cde78e264e29dd89138b50c2
nasim <admin@xeba.co.nz>
2016-04-08 09:32:13 +1200

    New changelist

---
+++ application/views/home.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!doctype html>
+<html>
+<head>
+	<title>TEST</title>
+	<!--build:css css/styles.min.css -->
+	<!-- bower:css -->
+
+	<!-- endbower -->
+	<!-- endbuild -->
+</head>
+<body>
+<h1>Hello Beshad :)</h1>
+<!--build:js js/main.min.js -->
+<!-- bower:js -->
+
+<!-- endbower -->
+<!-- endbuild -->
+</body>
+</html>
