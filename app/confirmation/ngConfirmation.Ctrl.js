bf835755b20551a26405bfc6bc6c314c269554ad
Beshad Ghorbani <admin@xeba.co.nz>
2016-04-21 09:32:33 +1200

    New changelist

---
+++ app/confirmation/ngConfirmation.Ctrl.js
@@ -0,0 +1,13 @@
+(function () {
+
+    "user strict";
+
+    angular.module("myApp")
+        .controller('ngConfirmationCtrl', function ($scope, $http) {
+
+        });
+    /*.controller('ngDeviceSelectionCtrl', function ($scope, $http) */
+})();
+/*(function () {*/
+
+
