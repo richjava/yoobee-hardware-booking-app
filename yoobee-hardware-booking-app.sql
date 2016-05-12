6917bb68a9be0ca7242e9327e29c0d5903c3c745
Beshad Ghorbani <admin@xeba.co.nz>
2016-05-13 09:51:23 +1200

    New changelist

--- yoobee-hardware-booking-app.sql
+++ yoobee-hardware-booking-app.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.6.0
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: May 10, 2016 at 09:16 AM
--- Server version: 5.5.42
+-- Host: localhost
+-- Generation Time: May 12, 2016 at 09:50 PM
+-- Server version: 5.5.41-log
 -- PHP Version: 7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `devicebooking_db`
@@ -31,6 +37,14 @@ CREATE TABLE `bookings_tb` (
   ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
 
+--
+-- Dumping data for table `bookings_tb`
+--
+
+INSERT INTO `bookings_tb` (`booking_id`, `date_created`, `student_id`, `start_date`, `end_date`, `status`) VALUES
+  (18, ''2016 - 05 - 11 23:09:28'', 8, ''2016 - 05 - 17 12:00:00'', ''2016 - 05 - 19 12:00:00'',
+   ''AWAITING COLLECTION'');
+
 -- --------------------------------------------------------
 
 --
@@ -38,11 +52,10 @@ CREATE TABLE `bookings_tb` (
 --
 
 CREATE TABLE `categories_tb` (
-  `category_id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL
+  `category_id`   INT(11)      NOT NULL,
+  `category_name` VARCHAR(255) NOT NULL
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 6
   DEFAULT CHARSET = latin1;
 
 --
@@ -50,11 +63,11 @@ CREATE TABLE `categories_tb` (
 --
 
 INSERT INTO `categories_tb` (`category_id`, `category_name`) VALUES
-  (1, 'Cameras'),
-  (2, 'Flash photography'),
-  (3, ' Optical filters'),
-  (4, 'Photographic shutters'),
-  (5, 'Tripod');
+  (1, ''Cameras''),
+  (2, ''Flash photography''),
+  (3, '' Optical filters''),
+  (4, ''Photographic shutters''),
+  (5, ''Tripod'');
 
 -- --------------------------------------------------------
 
@@ -65,12 +78,11 @@ INSERT INTO `categories_tb` (`category_id`, `category_name`) VALUES
 CREATE TABLE `devices_tb` (
   `device_id`   INT(11) NOT NULL,
   `device_name` VARCHAR(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
   `category_id` INT(11)      DEFAULT NULL,
   `image_url`   VARCHAR(255) DEFAULT NULL
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 29
   DEFAULT CHARSET = latin1;
 
 --
@@ -78,25 +90,34 @@ CREATE TABLE `devices_tb` (
 --
 
 INSERT INTO `devices_tb` (`device_id`, `device_name`, `description`, `category_id`, `image_url`) VALUES
-  (1, 'Nikon Coolpix S97000',
-   'The Nikon Coolpix S9700 camera packs a 30x zoom lens into a compact body with Wi-Fi. And its images, even at high ISOs, are impressive.',
-   2, '12441-3.png'),
-  (2, 'Sony Cyber-shot DSC-RX10', 'The Sony Cyber-shot DSC-RX10 is a bridge-style, long-zoom camera that gets just about everything right, and its steep asking price reflects its impeccable quality.', 2, 'ba940-1.png'),
-  (5, 'Sony Alpha 6000', 'The Sony Alpha 6000 focuses instantly and shoots at 11.1fps. Its image quality matches its speed, making it our top pick for entry-level mirrorless cameras.', 3, '773be-2.png'),
-  (6, 'Olympus OM-D E-M1', 'With gorgeous images—even in low light, incredible speed, and a wealth of high-end features, the Olympus OM-D E-M1 is the best Micro Four Thirds camera that money can buy.', 3, 'd9603-7.png'),
-  (9, 'Nikon D5300', 'The Wi-Fi- and GPS-equipped Nikon D5300 D-SLR is a modest upgrade from the previous model, but it''s just as worthy', 4, 'de009-6.png'),
-  (10, 'Pentax K-3', 'The Pentax K-3 is the most refined K-mount SLR to date, offering fast autofocus and superb image quality. Its video autofocus implementation isn''t the best, but it still manages to snag our Editors'' Choice award.', 4, '44b38-1.png'),
-  (11, 'Canon EOS 6D', 'The Canon EOS 6D is a top-notch full-frame camera in a compact body. With a relatively affordable price, enthusiast-friendly features, and spectacular image quality, it''s an easy Editors'' Choice.', 5, 'c52c2-8.png'),
-  (12, 'Canon PowerShot Elph 330 HS ', 'One of the best cameras you can buy for less than $250, the svelte, 10x-zoom Canon PowerShot Elph 330 HS takes beautiful pictures and lets you send them instantaneously with integrated Wi-Fi.', 5, '704b6-3.png'),
-  (14, 'Olympus Tough TG-2 iHS', 'The Olympus Tough TG-2 iHS rugged camera is a very minor upgrade to the excellent TG-1. ', 1, '24126-9.png'),
-  (15, 'Samsung Galaxy S7 Edge', 'The gorgeous Galaxy S7 Edge makes the best Android phone that much better.', 1, '67c0d-10.png'),
-  (16, 'Apple iPhone 6s Plus', 'The iPhone 6S Plus has a few key advantages that give it an edge for serious iPhone users, but its big body still may not fit for a lot of people.', 2, '99063-5.png'),
-  (17, 'Samsung Galaxy Note 5',
-   'Samsung''s Galaxy Note 5 is excellent overall, and the only phone to buy if you want to write by hand. However, you''ll pay a huge premium for a modest upgrade from last year''s model, and less pricey competitors will satisfy many.',
-   3, 'd75e1-9.png'),
-  (18, 'LG V10', 'Packed to the brim with features, the V10 is LG''s best smartphone yet. But if you don''t need two front cameras and dual displays, look elsewhere for something less expensive.', 4, '109b2-3.png'),
-  (25, 'Samsung Galaxy S7 Edge', 'The gorgeous Galaxy S7 Edge makes the best Android phone that much better.', 5,
-   'e6ef5-3.png');
+  (1, ''Nikon Coolpix S97000'',
+   ''The Nikon Coolpix S9700 camera packs a 30x zoom lens INTO a COMPACT body WITH Wi-Fi.AND its images,
+   even AT high ISOs, are impressive.'', 2, ''12441 - 3.png''),
+  (2, ''Sony Cyber - shot DSC - RX10'', ''The Sony Cyber-shot DSC-RX10 IS a bridge - style, LONG - zoom camera that gets just about everything RIGHT, AND its steep asking price reflects its impeccable quality.'', 2, ''ba940 - 1.png''),
+  (5, ''Sony Alpha 6000'', ''The Sony Alpha 6000 focuses instantly AND shoots AT 11.1fps.Its image quality matches its speed, making it our top pick FOR entry- LEVEL mirrorless cameras.'', 3, ''773be - 2.png''),
+  (6, ''Olympus OM - D E - M1'', ''WITH gorgeous images—even IN low light, incredible speed, AND a wealth of high - END features, the Olympus OM-D E-M1 IS the best Micro Four Thirds camera that money can buy.'', 3, ''d9603 - 7.png''),
+  (9, ''Nikon D5300'', ''The Wi-Fi- AND GPS-equipped Nikon D5300 D-SLR IS a modest UPGRADE FROM the previous model, but it\''s just AS worthy'', 4, ''de009 - 6.png''),
+  (10, ''Pentax K - 3'', ''The Pentax K-3 IS the most refined K-mount SLR TO DATE, offering FAST autofocus AND superb image quality.Its video autofocus implementation isn\''t the best, but it still manages TO snag our Editors\'' Choice award.'', 4, ''44b38 - 1.png''),
+  (11, ''Canon EOS 6D'', ''The Canon EOS 6D IS a top-notch FULL -frame camera IN a COMPACT body.WITH a relatively affordable price,
+   enthusiast - friendly
+   features, AND spectacular image quality, it\''s an easy Editors\'' Choice.'', 5, ''c52c2 - 8.png''),
+  (12, ''Canon PowerShot Elph 330 HS '', ''ONE of the best cameras you can buy FOR LESS THAN $250, the svelte, 10x -
+                                                                                                               zoom
+                                                                                                               Canon
+      PowerShot Elph 330 HS takes beautiful pictures AND lets you send them instantaneously WITH integrated Wi-Fi.
+                                                                  '', 5, ''704b6 - 3.png''),
+  (14, ''Olympus Tough TG - 2 iHS'', ''The Olympus Tough TG-2 iHS rugged camera IS a very minor UPGRADE TO the excellent TG-1. '', 1, ''24126 - 9.png''),
+  (15, ''Samsung Galaxy S7 Edge'', ''The gorgeous Galaxy S7 Edge makes the best Android phone that much better.'', 1, ''67c0d - 10.png''),
+  (16, ''Apple iPhone 6s Plus'', ''The iPhone 6S Plus has a few KEY advantages that give it an edge FOR serious iPhone users, but its big body still may NOT fit FOR a lot of people.'', 2, ''99063 - 5.png''),
+  (17, ''Samsung Galaxy Note 5'', ''Samsung\''s Galaxy Note 5 IS excellent overall, AND the only phone TO buy IF you
+                                                                                        want TO WRITE BY hand.However,
+   you\''ll pay a huge premium FOR a modest UPGRADE FROM LAST YEAR \''s model, AND LESS pricey competitors will satisfy
+   many.'', 3, ''d75e1 - 9.png''),
+  (18, ''LG V10'', ''Packed TO the brim WITH features,
+   the V10 IS LG\''s best smartphone yet.But IF you don\''t need two front cameras AND DUAL displays,
+   look elsewhere FOR something LESS expensive.'', 4, ''109b2 - 3.png''),
+  (25, ''Samsung Galaxy S7 Edge'', ''The gorgeous Galaxy S7 Edge makes the best Android phone that much better.'', 5,
+   ''e6ef5 - 3.png'');
 
 -- --------------------------------------------------------
 
@@ -105,8 +126,8 @@ INSERT INTO `devices_tb` (`device_id`, `device_name`, `description`, `category_i
 --
 
 CREATE TABLE `programmes_tb` (
-  `programme_id` int(11) NOT NULL,
-  `programme_name` varchar(255) NOT NULL
+  `programme_id`   INT(11)      NOT NULL,
+  `programme_name` VARCHAR(255) NOT NULL
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
@@ -125,6 +146,13 @@ CREATE TABLE `selected_devices_tb` (
   ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
 
+--
+-- Dumping data for table `selected_devices_tb`
+--
+
+INSERT INTO `selected_devices_tb` (`selected_devices_id`, `booking_id`, `device_id`) VALUES
+  (18, 18, 14);
+
 -- --------------------------------------------------------
 
 --
@@ -140,10 +168,9 @@ CREATE TABLE `students_tb` (
   `phone`        VARCHAR(255) NOT NULL,
   `email`        VARCHAR(255) NOT NULL,
   `programme_id` INT(11)               DEFAULT NULL,
-  `role`         VARCHAR(11)  NOT NULL DEFAULT 'student'
+  `role`         VARCHAR(11)  NOT NULL DEFAULT ''student''
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 8
   DEFAULT CHARSET = latin1;
 
 --
@@ -152,9 +179,8 @@ CREATE TABLE `students_tb` (
 
 INSERT INTO `students_tb` (`student_id`, `username`, `password`, `fullname`, `address`, `phone`, `email`, `programme_id`, `role`)
 VALUES
-  (6, 'beshad', '12345', 'arshia Ghorbani', '34 Rockview Place, Mount Pleasant, Christchurch', '021-2775060',
-   'admin@xeba.co.nz', NULL, 'admin'),
-  (7, 'arshia', '12345', '', '', '', '', NULL, 'student');
+  (8, ''beshad'', ''8cb2237d0679ca88db6464eac60da96345513964'', ''beshad ghorbani'', ''34 rockview place,
+      mount pleasant, Christchurch 8081'', ''021-2775060'', ''admin@xeba.co.nz'', NULL, ''admin'');
 
 --
 -- Indexes for dumped tables
@@ -205,7 +231,7 @@ ADD PRIMARY KEY (`student_id`);
 -- AUTO_INCREMENT for table `bookings_tb`
 --
 ALTER TABLE `bookings_tb`
-MODIFY `booking_id` INT(11) NOT NULL AUTO_INCREMENT;
+MODIFY `booking_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 19;
 --
 -- AUTO_INCREMENT for table `categories_tb`
 --
@@ -215,7 +241,7 @@ MODIFY `category_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 6;
 -- AUTO_INCREMENT for table `devices_tb`
 --
 ALTER TABLE `devices_tb`
-MODIFY `device_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 29;
+MODIFY `device_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 26;
 --
 -- AUTO_INCREMENT for table `programmes_tb`
 --
@@ -225,9 +251,12 @@ MODIFY `programme_id` INT(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `selected_devices_tb`
 --
 ALTER TABLE `selected_devices_tb`
-MODIFY `selected_devices_id` INT(11) NOT NULL AUTO_INCREMENT;
+MODIFY `selected_devices_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 19;
 --
 -- AUTO_INCREMENT for table `students_tb`
 --
 ALTER TABLE `students_tb`
-MODIFY `student_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 8;
+MODIFY `student_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 9;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
